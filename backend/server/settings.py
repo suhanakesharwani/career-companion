43489a3d648b0bb4f8845e1ed94240a961ec848b
Suhana <suhanakesharwani@gmail.com>
2026-04-11 03:47:07 +0530

    Refactor settings to load Redis URL from environment variables

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -255,7 +255,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.getenv("REDIS_URL", "redis://127.0.0.1:6379/0")]
+            "hosts": [os.getenv("REDIS_URL")]
         },
     },
 }
