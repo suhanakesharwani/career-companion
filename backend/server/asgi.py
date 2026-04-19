c095227df35b46110cecb4339c0d8b0c966ee1c1
Suhana <suhanakesharwani@gmail.com>
2026-04-20 02:06:47 +0530

    fix asgi issue

--- backend/server/asgi.py
+++ backend/server/asgi.py
@@ -2,10 +2,12 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from ai_interview.routing import websocket_urlpatterns
-
+import ai_interview.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": URLRouter(websocket_urlpatterns),
+    "websocket": URLRouter(
+        
+        ai_interview.routing.websocket_urlpatterns),
 })
