8daa79ef15da34972fc422bd110e20f6924c25ff
Suhana <suhanakesharwani@gmail.com>
2026-04-20 03:36:48 +0530

    remove custom middleware from asgi

--- backend/server/asgi.py
+++ backend/server/asgi.py
@@ -1,17 +1,16 @@
-from channels.auth import AuthMiddlewareStack
-from ai_interview.middleware import JWTAuthMiddleware
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 import ai_interview.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-
-    "websocket": JWTAuthMiddleware(
-        URLRouter(
-            ai_interview.routing.websocket_urlpatterns
-        )
+    "http": django_asgi_app,
+    "websocket": URLRouter(
+        ai_interview.routing.websocket_urlpatterns
     ),
 })
