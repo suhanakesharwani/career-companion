c5c9969fe897acff20b84bd1ee0ce115b7ea4cd4
Suhana <suhanakesharwani@gmail.com>
2026-04-27 05:31:34 +0530

    write ai-service url

--- backend/matching/ai_client.py
+++ backend/matching/ai_client.py
@@ -1,7 +1,7 @@
 import requests
 
 # Paste your AI service URL here
-AI_SERVICE_URL = "https://your-ai-app.onrender.com/match"
+AI_SERVICE_URL = "https://career-companion-ai-v1rj.onrender.com/match"
 session = requests.Session()
 
 def get_skill_match(jd_skills, resume_skills):
--- backend/server/urls.py
+++ backend/server/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-import debug_toolbar
+# import debug_toolbar
 
 from rest_framework_simplejwt.views import(
     TokenObtainPairView,
